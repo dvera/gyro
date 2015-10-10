c3d8e474f87139bc0281d960cbbcf1d279c2f94d
Daniel Vera <dlv04c@spear-14.rcc.fsu.edu>
2015-10-10 06:26:39 -0400

    fixed bug in closest

---
+++ R/gyroUpdate.R
@@ -0,0 +1,10 @@
+gyroUpdate<-function(message="no message"){
+	library(rubber)
+	res <- system(paste0("git -C /lustre/maize/home/dlv04c/software/r/gyro/ add /lustre/maize/home/dlv04c/software/r/gyro/ &&\
+	git -C /lustre/maize/home/dlv04c/software/r/gyro/ commit -a -m '",message,"' &&\
+	git -C /lustre/maize/home/dlv04c/software/r/gyro/ push"))
+	library(devtools)
+	detach("package:gyro",unload=T)
+	install_github("dvera/gyro")
+	library(rubber)
+}
