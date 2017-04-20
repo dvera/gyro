8ad4e13dae7126027b7a37ba6fed14915bba4015
Daniel Vera <vera@genomics.fsu.edu>
2017-04-20 06:42:08 -0400

    Create samtoolsRmdup.R

---
+++ R/samtoolsRmdup.R
@@ -0,0 +1,26 @@
+samtoolsRmdup <- function( inFiles , singleEnd=FALSE , pairedAsSingle=FALSE , outputFormat="BAM"threads=getOption("threads",1L) ){
+  
+  # generate output file names
+  outnames <- paste0(
+      basename(removeext(inFiles)),
+      if( singleEnd ){ "_singleEnd" },
+      if( pairedAsSingle ){ "_pairedAsSingle" },
+      "_rmdup.",
+      tolower(outputFormat)
+    )
+    
+   # generate command string
+  cmdString <- paste(
+    "samtools rmdup",
+    if( pairedAsSingle ){ "-S" },
+    if( singleEnd ){ "-s" },
+    inFiles,
+    ">", outnames
+  )
+    
+  # print and execute command string
+  res <- cmdRun(cmdString,threads)
+
+  return(outnames)
+  
+}
